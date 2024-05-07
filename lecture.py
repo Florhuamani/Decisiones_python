d535cb9c602779e87d90e791caedc60d3d27494c
Florlucana <lucanahuamaniflormilagros@gmail.com>
2024-05-07 12:45:21 -0500

    cree mi archivo de lecture.py en la carpeta de decisiones de python el dia 07-05-2024

---
+++ lecture.py
@@ -0,0 +1,29 @@
+## Utilizando una estructura "if-else":
+```python
+edad = 18
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+```
+
+## Usando la declaración "if-elif-else":
+```python
+nota = 70
+if nota >= 90:
+    print("Sobresaliente")
+elif nota >= 80:
+    print("Notable")
+elif nota >= 70:
+    print("Aprobado")
+else:
+    print("Reprobado")
+```
+
+## Implementando un bucle "while":
+```python
+contador = 0
+while contador < 5:
+    print("Iteración:", contador)
+    contador += 1
+```
